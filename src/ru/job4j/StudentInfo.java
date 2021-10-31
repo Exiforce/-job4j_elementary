be65f7ca8fd105b4eb146dd3139209c2a6b9d4d3
Exiforce <java.program@mail.r>
2021-10-31 23:20:40 +1000

    3. Арифметические операции. [#185]

--- src/ru/job4j/StudentInfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Artur Skuba");
-        System.out.println("19.06.1987");
-    }
-}
