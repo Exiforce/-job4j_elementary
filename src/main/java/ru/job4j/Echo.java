ee4dd23f64f80acec06c03162e456529a5fe61b9
Exiforce <java.program@mail.r>
2021-10-31 19:12:24 +1000

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -1,4 +1,10 @@
 package ru.job4j;
 
 public class Echo {
+    public static class StudentInfo {
+        public static void main(String[]  args) {
+    System.out.println("Artur  Skuba");
+    System.out.println("19.06.1987");
+          }
+        }
 }
