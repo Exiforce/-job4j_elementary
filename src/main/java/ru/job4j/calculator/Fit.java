60848c8b87a02e76eb1e8cce57005c26e25711a0
Exiforce <java.program@mail.r>
2021-11-01 20:02:27 +1000

    6. Идеальный вес [#41584 #223109]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -5,9 +5,11 @@ public class Fit {
     public static double manWeight(short height) {
         return (height - 100)  * 1.15;
     }
+
     public static double womanWeight(short height) {
         return (height - 110) * 1.15;
     }
+
     public static void main(String[] args) {
         short height = 182;
         double man = Fit.manWeight(height);
