210b12c4d8901dc51052ab038c7488268c153962
Exiforce <java.program@mail.r>
2021-11-03 22:54:52 +1000

    9.1. String [#237407 #223741]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -3,7 +3,11 @@ package ru.job4j.condition;
 public class Greeting {
     public static void main(String[] args) {
         String idea  = "I like Java!";
+        System.out.println(idea);
+        String id = idea + " But I am a newbie. ";
+        System.out.println(id);
         int year = 2021;
-        System.out.println(idea + " But I am a newbie. " + year);
-   }
+        String i = id + year;
+        System.out.println(i);
+    }
 }
