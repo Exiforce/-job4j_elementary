0ab40ce418e2c2cd26da34b6598f7670cb7ae0da
Exiforce <java.program@mail.r>
2021-11-05 01:26:38 +1000

    1.1. Что такое import [#504773]

---
+++ src/main/java/ru/job4j/math/MathCalculator.java
@@ -0,0 +1,29 @@
+package ru.job4j.math;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double minusAndDel(double first, double second) {
+        return minus(first, second)
+                + del(first, second);
+    }
+
+    public static double sumAndAll(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second)
+                + minus(first, second)
+                + del(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + minusAndDel(10, 20));
+        System.out.println("Результат расчета равен: " + sumAndAll(10, 20));
+    }
+}
