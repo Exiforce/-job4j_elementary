b9267dcb0ce8f42c7fad1a80d29b387b8c235bc3
Exiforce <java.program@mail.r>
2021-11-05 11:39:28 +1000

    0. Что такое тестирование [#159431 #224509]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -7,23 +7,23 @@ public class Converter {
     }
 
     public static float rubleToDollar(float value) {
-        return value / 64;
+        return value / 60;
     }
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
-        float dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + dollar + " dollar.");
+        float dollar = Converter.rubleToDollar(180);
+        System.out.println("180 rubles are " + dollar + " dollar.");
         float in = 140;
         float expected = 2;
         float out = Converter.rubleToEuro(in);
         boolean passed = expected == out;
         System.out.println("140 rubles are 2 euro. Test result : " + passed);
-        float in2 = 140;
-        double expected2 = 2.1875;
+        float in2 = 180;
+        double expected2 = 3;
         float out2 = Converter.rubleToDollar(in2);
         boolean passed2 = expected2 == out2;
-        System.out.println("140 rubles are 2.1875 dollar. Test result : " + passed2);
+        System.out.println("180 rubles are 3 dollar. Test result : " + passed2);
     }
 }
