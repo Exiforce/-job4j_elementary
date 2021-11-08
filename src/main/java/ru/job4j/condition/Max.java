7ea9254fd5a6b899eea70ea52a6e143725eb5391
Exiforce <java.program@mail.r>
2021-11-08 10:29:37 +1000

    4.2. Тернарное сравнение [#189 #225724]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -3,8 +3,7 @@ package ru.job4j.condition;
 public class Max {
 
     public static int max(int left, int right)  {
-        boolean rsl  =  left > right;
-        return rsl ? left : right;
+        return left > right ? left : right;
     }
 
     public  static  void main(String[] args) {
