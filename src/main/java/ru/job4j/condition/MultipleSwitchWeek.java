f262e2c2664b45f6808ef54d528710870ab65c96
Exiforce <java.program@mail.r>
2021-11-09 14:51:48 +1000

    4.6.2. Switch - matcher [#334071]

---
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class MultipleSwitchWeek {
+    public static int namberOfDay(String name) {
+        return switch (name) {
+            case "Понедельник", "Monday" -> 1;
+            case "Вторник", "Tuesday" -> 2;
+            case "Среда", "Wednesday" -> 3;
+            case "Четверг", "Thursday" -> 4;
+            case "Пятница", "Friday" -> 5;
+            case "Суббота", "Saturday" -> 6;
+            case "Воскресение", "Sunday" -> 7;
+            default -> 0;
+        };
+    }
+
+    public static  void main(String[] args) {
+        int rsl = MultipleSwitchWeek.namberOfDay("Понедельник");
+        System.out.println(rsl);
+    }
+}
