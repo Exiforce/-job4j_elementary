4c0efe671a79f5a696207bf0fe02ca6b02f29489
Exiforce <java.program@mail.r>
2021-11-11 22:55:40 +1000

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -10,9 +10,24 @@ public class Counter {
         return total;
     }
 
+    public static int sumByEven(int first, int finish) {
+        int total = 0;
+        int sum;
+        for (sum = first; sum <= finish; sum++) {
+            if (sum % 2 == 0) {
+                total = total + sum;
+            }
+        }
+        return total;
+    }
+
     public  static void main(String[] args)  {
         System.out.println("Сумма всех чисел в цикле = " + sum(0, 10));
         System.out.println("Сумма всех чисел в цикле = " + sum(3, 8));
         System.out.println("Сумма всех чисел в цикле = " + sum(1, 1));
+
+        System.out.println("Сумма всех четных чисел в цикле = " + sumByEven(0, 10));
+        System.out.println("Сумма всех четных чисел в цикле = " + sumByEven(3, 8));
+        System.out.println("Сумма всех четных чисел в цикле = " + sumByEven(1, 1));
     }
 }
