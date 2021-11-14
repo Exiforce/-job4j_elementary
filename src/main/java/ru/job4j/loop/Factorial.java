00dcb93689f4909b7099fceaf33a6f6b0b301f8b
Exiforce <java.program@mail.r>
2021-11-14 21:09:29 +1000

    5.2. Создать программу, вычисляющую факториал. [#193 #227659]

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -2,10 +2,9 @@ package ru.job4j.loop;
 
 public class Factorial {
     public static int calc(int n) {
-        int x;
         int result = 1;
-        for (x = 1; x <= n; x++) {
-            result = result * x;
+        for (int x = 1; x <= n; x++) {
+            result *= x;
         }
     return result;
     }
