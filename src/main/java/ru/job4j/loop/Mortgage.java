ecf5dc973a6f9012a84df16cda493c2eb84c40bf
Exiforce <java.program@mail.r>
2021-11-16 11:20:38 +1000

    5.7. Ипотека [#156315 #228742]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,8 +4,7 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            int credit = salary - (int) (amount * percent / 100);
-            amount -= credit;
+            amount += amount * (percent / 100) - salary;
             year++;
         }
         return year;
