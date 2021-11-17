0fa67f29f3d2360edd7060b7e3205695eee300ef
Exiforce <java.program@mail.r>
2021-11-17 11:25:40 +1000

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,18 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Ivan";
+        names[1] = "Andrey";
+        names[2] = "Ilya";
+        names[3] = "Igor";
+        String name1 = names[0];
+        String name2 = names[1];
+        String name3 = names[2];
+        String name4 = names[3];
+        System.out.println(name1);
+        System.out.println(name2);
+        System.out.println(name3);
+        System.out.println(name4);
     }
 }
