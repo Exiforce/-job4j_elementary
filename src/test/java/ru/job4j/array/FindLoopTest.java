e6b2f35bf21c7d866644771249b05fdefb470c47
Exiforce <java.program@mail.r>
2021-11-23 21:53:16 +1000

    6.1. Классический поиск перебором. [#33489 #229461]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -5,33 +5,6 @@ import org.junit.Test;
 
 public class FindLoopTest {
     @Test
-    public void whenArrayHas6ThenMinus1() {
-        int[] data = {0, 1, 2, 3};
-        int el = 6;
-        int result = FindLoop.indexOf(data, el);
-        int expected = -1;
-        Assert.assertEquals(expected, result);
-    }
-
-    @Test
-    public void whenArrayHas9Then9() {
-        int[] data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int el = 9;
-        int result = FindLoop.indexOf(data, el);
-        int expected = 9;
-        Assert.assertEquals(expected, result);
-    }
-
-    @Test
-    public void whenArrayHasLength5Then0() {
-        int[] data = new int[] {5, 10, 3};
-        int el = 5;
-        int result = FindLoop.indexOf(data, el);
-        int expected = 0;
-        Assert.assertEquals(expected, result);
-    }
-
-    @Test
     public void whenFind3() {
         int[] data = new int[] {5, 2, 10, 2, 4};
         int el = 2;
