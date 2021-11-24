29939c894cadd30520577bfcd23640767cb9c11d
Exiforce <java.program@mail.r>
2021-11-24 10:35:04 +1000

    6.1. Классический поиск перебором. [#33489 #229461]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,15 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int index(int[] data, int el) {
+        int rst = -1;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                rst = i;
+                break;
+            }
+        }
+        return rst;
+    }
 }
--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -5,23 +5,28 @@ import org.junit.Test;
 
 public class FindLoopTest {
     @Test
-    public void whenFind3() {
-        int[] data = new int[] {5, 2, 10, 2, 4};
-        int el = 2;
-        int start = 2;
-        int finish = 4;
-        int result = FindLoop.indexOf(data, el, start, finish);
-        int expected = 3;
+    public void whenArrayHas5Then0() {
+        int[] data = {5, 4, 3, 2};
+        int el = 5;
+        int result = FindLoop.index(data, el);
+        int expected = 0;
         Assert.assertEquals(expected, result);
     }
 
     @Test
-    public void whenNoFindMinus1() {
-        int[] data = new int[] {8, 7, 6, 9, 55, 14, 1, 17, 8, 5};
-        int el = 8;
-        int start = 2;
-        int finish = 7;
-        int result = FindLoop.indexOf(data, el, start, finish);
+    public void whenArrayHas3Then2() {
+        int[] data = {1, 2, 3, 4, 5, 6};
+        int el = 3;
+        int result = FindLoop.index(data, el);
+        int expected = 2;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHasNo6ThenMinus1() {
+        int[] data = {13, 4, 1, 9, 8, 98};
+        int el = 6;
+        int result = FindLoop.index(data, el);
         int expected = -1;
         Assert.assertEquals(expected, result);
     }
