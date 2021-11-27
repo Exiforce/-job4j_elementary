a0de9ad44535d7978434fa43c4f2c63388a90350
Exiforce <java.program@mail.r>
2021-11-28 01:41:15 +1000

    6.7.4. Выигрышные комбинации в сокобан [#53859 #232719]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,6 +1,6 @@
 package ru.job4j.array;
 
-public class MatrixCheck {
+public class MatrixCheck<rsl> {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
@@ -33,12 +33,10 @@ public class MatrixCheck {
 
     public static boolean isWin(char[][] board) {
         boolean rsl = false;
-        for (int i = 0; i < 5; i++) {
-            if (board[i][i] == 'X') {
-                if ((monoHorizontal(board, i) || monoVertical(board, i))) {
-                    rsl = true;
-                    break;
-                }
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X' && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                rsl = true;
+                break;
             }
         }
         return rsl;
